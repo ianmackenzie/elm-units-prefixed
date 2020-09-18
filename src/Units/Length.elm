55228b022d4ee748956b6205c33888066173575a
Ian Mackenzie <mackenzie@arrival.com>
2020-09-18 16:56:17 -0400

    Bump to 2.4.0

--- src/Units/Length.elm
+++ src/Units/Length.elm
@@ -1,4 +1,4 @@
-module Units.Length exposing (Length, Meters, meters, inMeters, microns, inMicrons, millimeters, inMillimeters, centimeters, inCentimeters, kilometers, inKilometers, thou, inThou, inches, inInches, feet, inFeet, yards, inYards, miles, inMiles, astronomicalUnits, inAstronomicalUnits, parsecs, inParsecs, lightYears, inLightYears, meter, micron, millimeter, centimeter, kilometer, inch, foot, yard, mile, astronomicalUnit, parsec, lightYear)
+module Units.Length exposing (Length, Meters, meters, inMeters, microns, inMicrons, millimeters, inMillimeters, centimeters, inCentimeters, kilometers, inKilometers, thou, inThou, inches, inInches, feet, inFeet, yards, inYards, miles, inMiles, cssPixels, inCssPixels, points, inPoints, picas, inPicas, astronomicalUnits, inAstronomicalUnits, parsecs, inParsecs, lightYears, inLightYears, meter, micron, millimeter, centimeter, kilometer, inch, foot, yard, mile, astronomicalUnit, parsec, lightYear)
 
 {-| A `Length` represents a length in meters, feet, centimeters, miles etc. It
 is stored as a number of meters.
@@ -15,6 +15,11 @@ is stored as a number of meters.
 ## Imperial
 
 @docs thou, inThou, inches, inInches, feet, inFeet, yards, inYards, miles, inMiles
+
+
+## CSS and typography
+
+@docs cssPixels, inCssPixels, points, inPoints, picas, inPicas
 
 
 ## Astronomical
@@ -213,6 +218,68 @@ miles =
 inMiles : Length.Length -> Float
 inMiles =
   Length.inMiles
+
+
+{-| Construct a length from a number of [CSS pixels](https://drafts.csswg.org/css-values-3/#absolute-lengths),
+defined as 1/96 of an inch.
+
+Note the difference between this function and [`Pixels.pixels`](Pixels#pixels).
+`Length.cssPixels 1` is equivalent to `Length.inches (1 / 96)` or
+approximately `Length.millimeters 0.264583`; it returns a length in _real world_
+units equal to the (nominal) physical size of one CSS pixel.
+
+In contrast, `Pixels.pixels 1` simply returns an abstract "1 pixel" value. You
+can think of `Length.cssPixels 1` as a shorthand for
+
+    Pixels.pixels 1
+        |> Quantity.at_
+            (Pixels.pixels 96
+                |> Quantity.per (Length.inches 1)
+            )
+
+That is, `Length.cssPixels 1` is the size of 1 pixel at a resolution of 96 DPI.
+
+-}
+cssPixels : Float -> Length.Length
+cssPixels =
+  Length.cssPixels
+
+
+{-| Convert a length to a number of CSS pixels.
+-}
+inCssPixels : Length.Length -> Float
+inCssPixels =
+  Length.inCssPixels
+
+
+{-| Construct a length from a number of [points](https://en.wikipedia.org/wiki/Point_%28typography%29),
+defined as 1/72 of an inch.
+-}
+points : Float -> Length.Length
+points =
+  Length.points
+
+
+{-| Convert a length to a number of points.
+-}
+inPoints : Length.Length -> Float
+inPoints =
+  Length.inPoints
+
+
+{-| Construct a length from a number of [picas](https://en.wikipedia.org/wiki/Pica_%28typography%29),
+defined as 1/6 of an inch.
+-}
+picas : Float -> Length.Length
+picas =
+  Length.picas
+
+
+{-| Convert a length to a number of picas.
+-}
+inPicas : Length.Length -> Float
+inPicas =
+  Length.inPicas
 
 
 {-| Construct a length from a number of [astronomical units][au] (AU). One AU is
